9a6a5e1f3d7265403f4ab7877c5816426085770d
YashGuptadev21 <145590788+YashGuptadev21@users.noreply.github.com>
2023-11-14 05:41:51 +0000

    container config done

---
+++ 01_basics/Variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountName = "Yash"
+var accountPassword = "232123"
+accountCity = "Maharashtra"
+
+accountName = "kunal"
+accountPassword = "121312"
+accountCity = "Nagpur"
+
+console.table([accountId,accountName,accountPassword,accountCity]);
